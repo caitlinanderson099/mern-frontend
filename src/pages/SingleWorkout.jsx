81ca5f548f57bbda671aa4d953d135815850f265
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-08-20 11:47:37 +1200

    tidied up singleworkout page, finalised frontend

--- src/pages/SingleWorkout.jsx
+++ src/pages/SingleWorkout.jsx
@@ -10,12 +10,6 @@ const SingleWorkout = () => {
         <button onClick={() => navigate(-1)} className='back-button'> Back </button>
         <div>
             <h1> Single Workout Page || Work In Progress <ConeStriped/> </h1>
-            <h2> Workout Title: "Placeholder"</h2>
-            <h3> Reps: "Placeholder"</h3>
-            <h3> Load: "Placeholder"</h3>
-            <h3> Created At: "Placeholder"</h3>
-            <h3> Edited At: "Placeholder"</h3>
-            <h3> Created By: "Placeholder"</h3>
         </div>
     </>
   )
